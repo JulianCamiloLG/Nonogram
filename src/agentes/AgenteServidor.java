1ecca45ba207a6073db24dd019787dd8402879b7
JulianCamiloLG <julycamy_125@hotmail.com>
2019-03-31 02:19:47 -0500

    Carga inicial, estructura basica

---
+++ src/agentes/AgenteServidor.java
@@ -0,0 +1,155 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package agentes;
+
+import jade.core.Agent;
+import jade.core.behaviours.SimpleBehaviour;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author JulianCamilo
+ */
+public class AgenteServidor extends Agent {
+
+    private ComportamientoNonogramaNormal comportamientoNormal;
+    private ComportamientoNonogramaInverso comportamientoInverso;
+
+    @Override
+    protected void setup() {
+
+        String archivo = (String) this.getArguments()[0];
+        int modo = Integer.parseInt(this.getArguments()[1].toString());
+        System.out.println("Bienvenido, estamos preparandolo todo para resolver tu nonograma");
+        if (modo == 0) {
+            System.out.println("Procesando nonograma: " + archivo + " en modo normal");
+            comportamientoNormal = new ComportamientoNonogramaNormal(archivo);
+            addBehaviour(comportamientoNormal);
+        } else {
+            System.out.println("Procesando nonograma: " + archivo + " en modo inverso");
+            comportamientoInverso = new ComportamientoNonogramaInverso(archivo);
+            addBehaviour(comportamientoInverso);
+        }
+    }
+
+    @Override
+    protected void takeDown() {
+        System.out.println("Gracias, vuelve pronto");
+    }
+
+    public class ComportamientoNonogramaNormal extends SimpleBehaviour {
+
+        private final String archivo;
+        private LinkedList<String> filas;
+        private LinkedList<String> columnas;
+
+        public ComportamientoNonogramaNormal(String archivo) {
+            this.archivo = archivo;
+        }
+
+        private void leerArchivo() {
+            LinkedList<String> valores = new LinkedList();
+            File file = new File("C:\\Users\\JulianCamilo\\Documents\\NetBeansProjects\\Nonograma\\src\\archivos\\" + archivo);
+            try {
+                Scanner lector = new Scanner(file);
+                while (lector.hasNextLine()) {
+                    valores.add(lector.nextLine());
+                }
+            } catch (FileNotFoundException ex) {
+                System.out.println("No se encontro el archivo especificado");
+            }
+            filas = new LinkedList<>(valores.subList(0, valores.size() / 2));
+            columnas = new LinkedList<>(valores.subList((valores.size() / 2), valores.size()));
+            System.out.println("filas: " + filas);
+            System.out.println("columnas: " + columnas);
+        }
+
+        @Override
+        public void action() {
+            leerArchivo();
+        }
+
+        @Override
+        public boolean done() {
+            System.out.println("Termine de procesar");
+            return true;
+        }
+    }
+//#############################################################################################################    
+//#############################################################################################################    
+//#############################################################################################################    
+//#############################################################################################################    
+
+    public class ComportamientoNonogramaInverso extends SimpleBehaviour {
+
+        private final String archivo;
+        private char matriz[][];
+        private int limiteMatriz;
+
+        public ComportamientoNonogramaInverso(String archivo) {
+            this.archivo = archivo;
+        }
+
+        private void leerArchivo() {
+            LinkedList<String> filas = new LinkedList();
+            File file = new File("C:\\Users\\JulianCamilo\\Documents\\NetBeansProjects\\Nonograma\\src\\archivos\\" + archivo);
+            try {
+                Scanner lector = new Scanner(file);
+                while (lector.hasNext()) {
+                    filas.add(lector.next());
+                }
+            } catch (FileNotFoundException ex) {
+                System.out.println("No se encontro el archivo especificado");
+            }
+            System.out.println(filas);
+            llenarMatriz(filas);
+        }
+
+        private void llenarMatriz(LinkedList<String> filas) {
+            this.limiteMatriz = filas.size();
+            this.matriz = new char[limiteMatriz][limiteMatriz];
+            int cont=0;
+            for (String valore : filas) {
+               for (int i = 0; i < valore.length(); i++) {
+                    matriz[cont][i]=valore.charAt(i);
+                }
+                cont++; 
+            }
+                
+            imprimirMatriz();
+        }
+        
+        
+
+        public void imprimirMatriz() {
+            for (int i = 0; i < limiteMatriz; i++) {
+                for (int j = 0; j < limiteMatriz; j++) {
+                    System.out.print(matriz[i][j]);
+                }
+                System.out.println("");
+            }
+        }
+
+        @Override
+        public void action() {
+            leerArchivo();
+
+        }
+
+        @Override
+        public boolean done() {
+            System.out.println("Termine de procesar");
+            return true;
+        }
+
+    }
+
+}
---
+++ src/nonograma/Nonograma.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nonograma;
+
+import jade.Boot;
+
+/**
+ *
+ * @author JulianCamilo
+ */
+public class Nonograma {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        switch (args[0]){
+            case "-h":
+                mensajeAyuda();
+                break;
+            case "-i":
+                resolverInverso(args[1]);
+                break;
+            default:
+                resolverNonograma(args[0]);
+                break;
+        }
+    }
+    public static void mensajeAyuda(){
+        System.out.println("Programa creado por:");
+        System.out.println("Julian Camilo Lopez -- Sebastian Marulanda Sanchez");
+        System.out.println("---------------------------------------------------");
+        System.out.println("Lista de comandos:");
+        System.out.println("Ayuda: java -jar <ruta-de-jar> -h");
+        System.out.println("Resolver nonograma: java -jar <ruta-de-jar> <ruta-del-archivo.txt>");
+        System.out.println("Resolver nonograma inverso: java -jar <ruta-de-jar> -i <ruta-del-archivo.txt>");
+    }
+    
+    /**
+     * Método para lanzar el agente servidor en modo de
+     * nonograma normal
+     * param[1]= nombre-agente:clase-agente (parametros-agente separados por comas)
+     * @param ruta la ruta o el nombre del archivo a resolver
+     */
+    public static void resolverNonograma(String ruta){
+        String[] param = new String[2];
+        param[0] = "-gui";
+        param[1] = "AgenteServidor:agentes.AgenteServidor("
+                + ruta+",0)";
+        Boot.main(param);
+        
+    }
+    
+    /**
+     * Método para lanzar el agente servidor en modo de
+     * nonograma inverso
+     * @param ruta La ruta del nonograma inverso a resolver
+     */
+    public static void resolverInverso(String ruta){
+        String[] param = new String[2];
+        param[0] = "-gui";
+        param[1] = "AgenteServidor:agentes.AgenteServidor("
+                + ruta+",1)";
+        Boot.main(param);
+    }
+    
+}
